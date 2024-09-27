a744f648b0dab0c2025664d3f419b1b4e2d8372f
LetterC67 <hoangdeptoong@gmail.com>
2024-09-27 13:41:28 +0800

    Minor fix

--- frontend/src/components/Migrate.js
+++ frontend/src/components/Migrate.js
@@ -20,7 +20,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Check from "./../media/check.svg"
+
 function Migrate(){
 
     const [isModalOpen, setIsModalOpen] = useState(false);
